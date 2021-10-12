0379f0beeacbf27ca879e76b1d20eca0e49d78fc
Veerrohit Veeravadivel <veerrohitv@veerrohits-mbp.lan>
2021-10-12 07:35:37 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigator from './routes/homeStack';
+
+export default function App() {
+	return (
+		<>
+			<StatusBar style="auto" />
+			<Navigator />
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
